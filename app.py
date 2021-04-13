bcf4e1f87a5773a8ad43a940b7e86a3a23e2fdfe
Dominik1799 <dom.horvath17@gmail.com>
2021-04-13 13:53:23 +0200

    prepared stmnts

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from wtforms import StringField
 from wtforms.validators import InputRequired
 import psycopg2
+from psycopg2 import sql
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secKey'
@@ -14,24 +15,36 @@ class SearchForm(FlaskForm):
     search = StringField('Search for', [InputRequired()])
 
 
-@app.route("/search/<query>")
+@app.route("/unsafe_search/<query>")
 def unsafe_query(query):
     conn = psycopg2.connect(database="postgres", user="postgres",
-                           password="postgres", host="127.0.0.1", port="5432")
+                            password="postgres", host="127.0.0.1", port="5432")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM test")
-    print(cur.fetchall())
+    cur.execute("SELECT name, description, price FROM products WHERE name LIKE '%" + query + "%'")
+    items = cur.fetchall()
+    return render_template("search.html", items=items)
 
-    return render_template("search.html")
+
+@app.route("/safe_search/<query>")
+def safe_query(query):
+    conn = psycopg2.connect(database="postgres", user="postgres",
+                            password="postgres", host="127.0.0.1", port="5432")
+    q = sql.SQL("SELECT name, description, price FROM products WHERE name LIKE %s")
+    cur = conn.cursor()
+    cur.execute(q, ("%" + query + "%",))
+    items = cur.fetchall()
+    return render_template("search.html", items=items)
 
 
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
-    form = SearchForm()
-    if form.is_submitted():
-        func = "unsafe_query"
-        return redirect(url_for(func, query=request.form["search"]))
-    return render_template("index.html", form=form)
+    unsafe_form = SearchForm()
+    safe_form = SearchForm()
+    if "unsafe" in request.form and unsafe_form.is_submitted():
+        return redirect(url_for("unsafe_query", query=request.form["search"]))
+    if "safe" in request.form and safe_form.is_submitted():
+        return redirect(url_for("safe_query", query=request.form["search"]))
+    return render_template("index.html", unsafeForm=unsafe_form, safeForm=safe_form)
 
 
 if __name__ == '__main__':
